80bd808def21ab5ded719d6c39c396dd06552915
AntonTimberg <antonoffice2015@gmail.com>
2024-03-23 02:40:42 +0400

    - ref

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,11 @@ import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
         MyArrayList<Integer> myArrayList = new MyArrayList<>();
-        Iterator<Integer> iterator = myArrayList.iterator();
-
         for (int i = 0; i < 1000; i++) {
             myArrayList.add(i);
         }
 
+        Iterator<Integer> iterator = myArrayList.iterator();
         while (iterator.hasNext()) {
             Integer current = iterator.next();
             if (current % 2 == 0 || current % 3 == 0 || current % 5 == 0) {
