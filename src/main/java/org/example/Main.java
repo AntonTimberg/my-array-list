7cab2e2c5931538ce3c9f0c65fda2d3b5e719391
AntonTimberg <antonoffice2015@gmail.com>
2024-03-23 01:59:52 +0400

    -iterator func maintest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
+        MyArrayList<Integer> myArrayList = new MyArrayList<>();
+        Iterator<Integer> iterator = myArrayList.iterator();
 
+        for (int i = 0; i < 1000; i++) {
+            myArrayList.add(i);
+        }
+
+        while (iterator.hasNext()) {
+            Integer current = iterator.next();
+            if (current % 2 == 0 || current % 3 == 0 || current % 5 == 0) {
+                iterator.remove();
+            }
+        }
+
+        myArrayList.iterator().forEachRemaining(System.out::println);
     }
 }
